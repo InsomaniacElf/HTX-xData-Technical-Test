4f75f0a21b2e2344bbc4819412b2a0368c10d952
InsomaniacElf <79660779+InsomaniacElf@users.noreply.github.com>
2026-09-08 22:13:34 +0800

    load parakeet once

--- asr/asr_api.py
+++ asr/asr_api.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import time
+import tempfile
+import os
+
+import librosa
+import soundfile as sf
+import nemo.collections.asr as nemo_asr
 
 app = FastAPI()
+
+asr_model = nemo_asr.models.ASRModel.from_pretrained(
+    model_name="nvidia/parakeet-tdt-0.6b-v3"
+)
 
 @app.get("/ping")
 def ping():
